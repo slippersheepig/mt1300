2916163533fb69ffacde186f921a3371b9cf22bb
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-03 22:50:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
