5dc0f9664d2e438f18078646ae446f982768df87
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-06-11 22:57:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/slippersheepig/openwrt-passwall2.git;main" >> "feeds.conf.default"
